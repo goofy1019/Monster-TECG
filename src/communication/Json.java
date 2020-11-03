d17c1256dc4b15370996efefe9b27b3282decd26
Abner2111 <38987646+Abner2111@users.noreply.github.com>
2020-11-02 23:26:18 -0600

    Manejo de Json Iniciado Transcripcion de cartas a json Inicio de creacion de abstract factory para player (comunicacion)

---
+++ src/communication/Json.java
@@ -0,0 +1,21 @@
+package communication;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+public class Json {
+	private static ObjectMapper objectMapper = getDefaultObjectMapper();
+	
+	private static ObjectMapper getDefaultObjectMapper() {
+		ObjectMapper defaultObjectMapper = new ObjectMapper();
+		return defaultObjectMapper;
+	}
+	
+	public static JsonNode parse(String JsonSource) throws JsonMappingException, JsonProcessingException {
+		return objectMapper.readTree(JsonSource);
+	}
+
+}
