165b3d645b7a164f95165d5a1ecabf2e9ecb1ab5
Abner2111 <38987646+Abner2111@users.noreply.github.com>
2020-11-04 23:36:18 -0600

    Documentation added to ComsManager.java

--- src/communication/ComsManager.java
+++ src/communication/ComsManager.java
@@ -5,6 +5,12 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.InetAddress;
 
+/**
+ * This class' purpose is to provide communication between the two players of the game
+ * 
+ * @author Abner Arroyo y Adolfo Amador
+ *
+ */
 public class ComsManager {
 	
 	HostPlayer hplayer = null;
@@ -12,11 +18,18 @@ public class ComsManager {
 	DataOutputStream dos;
 	DataInputStream dis;
 	
+	/**
+	 * Constructor with port as parameter. It is created this way if the user selects to play as guest. It sets the 
+	 * Data output and input streams to math the selected player (Host or Guest)
+	 * @param PORT
+	 * @throws IOException
+	 */
 	public ComsManager(int PORT) throws IOException {
 		while(true) {
 			try {
-				hplayer = new HostPlayer(PORT);
-				
+				this.hplayer = new HostPlayer(PORT);
+				this.dos = hplayer.getDos();
+				this.dis = hplayer.getDis();
 				break;
 			} catch (IOException e){
 				e.printStackTrace();
@@ -24,11 +37,21 @@ public class ComsManager {
 		}
 	}
 	
+	/**
+	 * Constructor with ip and port as parameters. It is created this way if the user selects to play as guest. It sets the 
+	 * Data output and input streams to math the selected player (Host or Guest)
+	 * 
+	 * @param ip
+	 * @param PORT
+	 * @throws IOException
+	 */
 	public ComsManager(String ip, int PORT) throws IOException {
 		InetAddress def_ip = InetAddress.getByName(ip);
 		while(true) {
 			try {
-				gplayer = new GuestPlayer(def_ip, PORT);
+				this.gplayer = new GuestPlayer(def_ip, PORT);
+				this.dos = gplayer.getDos();
+				this.dis = gplayer.getDis();
 				break;
 			} catch (IOException e) {
 				e.printStackTrace();
@@ -36,6 +59,13 @@ public class ComsManager {
 		}	
 	}
 	
+	/**
+	 * This method sends a message through the DataOutPutStream
+	 * 
+	 * @param message
+	 * @return message
+	 * @throws IOException
+	 */
 	public String send(String message) throws IOException {
 		dos.writeUTF(message);
 		return message;
